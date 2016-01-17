618d07f49ad42bddd0ef26bf4d369e47a4032494
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-01-17 15:53:30 -0500

    Moved the format_priority and format_date methods into the listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,5 +1,28 @@
 module Listable
   def format_description(description)
-      "#{description}".ljust(30)
+    "#{description}".ljust(30)
+  end
+
+  def format_date(*args)
+    # format_date being used by the Todo Class
+    if args.length == 1
+      return args[0] ? args[0].strftime("%D") : "No due date"
+    end
+
+    # format_date being used by the Event Class
+    if args.length == 2
+      dates = @start_date.strftime("%D") if @start_date
+      dates << " -- " + @end_date.strftime("%D") if @end_date
+      dates = "N/A" if !dates
+      return dates
+    end
+  end
+
+  def format_priority
+    value = " ⇧" if @priority == "high"
+    value = " ⇨" if @priority == "medium"
+    value = " ⇩" if @priority == "low"
+    value = "" if !@priority
+    return value
   end
 end
