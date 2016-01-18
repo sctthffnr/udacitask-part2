b8d708fe4ba6d85c207bde898d08227098ae28f7
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-01-18 13:59:41 -0500

    Added error handling for invalid type and priority when adding to the list, and added on index check on the list's delete method.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,13 +5,19 @@ class UdaciList
     @title = options[:title]
     @items = []
   end
+  
   def add(type, description, options={})
     type = type.downcase
+    verify_type(type.downcase)
+    # Only verify priority if a priority value has been passed to the method
+    options[:priority] && verify_priority(options[:priority])
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
+
   def delete(index)
+    raise UdaciListErrors::IndexExceedsListSize if (index >= @items.length)
     @items.delete_at(index - 1)
   end
   def all
@@ -21,5 +27,17 @@ class UdaciList
     @items.each_with_index do |item, position|
       puts "#{position + 1}) #{item.details}"
     end
+  end
+
+  private
+
+  def verify_type(type)
+    valid_types = ["todo", "event", "link"]
+    raise UdaciListErrors::InvalidItemType unless valid_types.include?(type)
+  end
+
+  def verify_priority(priority)
+    valid_priorities = ["low", "medium", "high"]
+    raise UdaciListErrors::InvalidPriorityValue unless valid_priorities.include?(priority)
   end
 end
